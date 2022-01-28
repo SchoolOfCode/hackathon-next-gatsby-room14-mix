1fbc4005d55b7a3d6eb87b8b6f6e1f85ce20f65d
valeriocipolla <valeriocipolla92@gmail.com>
2022-01-28 16:08:15 +0000

    fetching name and img

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-import Layout from "../components/layout.js";
-import { StaticImage } from "gatsby-plugin-image";
+import Layout from "../components/layout/layout.js";
+import Image from "../components/image/index.js";
 
 const IndexPage = () => {
   const [data, setData] = useState(null);
@@ -20,13 +20,13 @@ const IndexPage = () => {
 
   return (
     <Layout pageTitle="Home Page">
-      <p>
-        We are room 14 and this is for the week 10 School of Code Hackathon.
-      </p>
-      <StaticImage
-        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
-        src="https://cdn2.thedogapi.com/images/w1Cal_vrT.jpg"
-      />
+      <p>{data ? data[0].breeds[0].name : "a name"}</p>
+      {data && (
+        <Image
+          alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+          src={data[0].url}
+        />
+      )}
     </Layout>
   );
 };
