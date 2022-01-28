203f578fb182f82e36cfe3ef70ee466f58ba6408
valeriocipolla <valeriocipolla92@gmail.com>
2022-01-28 12:09:30 +0000

    step 2 of tutorial finished

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,22 +1,27 @@
 import React from "react";
 import { Link } from "gatsby";
+import * as css from "./layout.module.css";
 
 const Layout = ({ pageTitle, children }) => {
   return (
-    <div>
+    <div className={css.container}>
       <title>{pageTitle}</title>
       <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
+        <ul className={css.navLinks}>
+          <li className={css.navLinkItem}>
+            <Link to="/" className={css.navLinkText}>
+              Home
+            </Link>
           </li>
-          <li>
-            <Link to="/about">About</Link>
+          <li className={css.navLinkItem}>
+            <Link to="/about" className={css.navLinkText}>
+              About
+            </Link>
           </li>
         </ul>
       </nav>
       <main>
-        <h1>{pageTitle}</h1>
+        <h1 className={css.heading}>{pageTitle}</h1>
         {children}
       </main>
     </div>
